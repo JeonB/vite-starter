8c9abd5457328aced662c68440a4c5f6e4b965eb
JeonB <newjeonb@gmail.com>
2024-04-13 18:51:55 +0900

    Add new files and code for React app setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <button className="red">Change to blue</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("button starts with correct color", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
+});
+test("button starts with correct text", () => {});
+test("button has correct color after click", () => {});
+test("button has correct text after click", () => {});
