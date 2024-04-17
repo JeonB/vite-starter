8d28c85df9d56049e9f48bc69912f24eb7888370
JeonB <newjeonb@gmail.com>
2024-04-17 22:47:56 +0900

    Update App.jsx and App.test.jsx with button disable functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 function App() {
+  const [disabled, setDisabled] = useState(false);
   return (
     <div>
       <button className="red">Change to blue</button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button starts with correct color", () => {
+// test("button starts with correct color", () => {
+//   render(<App />);
+//   const buttonElement = screen.getByRole("button", { name: /blue/i });
+//   expect(buttonElement).toHaveClass("red");
+// });
+// test("button starts with correct text", () => {});
+// test("button has correct color after click", () => {});
+// test("button has correct text after click", () => {});
+
+test("checkbox flow", () => {
   render(<App />);
+
+  // find elements
   const buttonElement = screen.getByRole("button", { name: /blue/i });
-  expect(buttonElement).toHaveClass("red");
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  // check initial conditions
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
 });
-test("button starts with correct text", () => {});
-test("button has correct color after click", () => {});
-test("button has correct text after click", () => {});
