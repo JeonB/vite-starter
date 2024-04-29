f461129ac74130acb9abfaf5dfb1bdebf2047090
JeonB <newjeonb@gmail.com>
2024-04-29 12:00:00 +0900

    Add unit test

--- src/pages/tests/SummaryForm.test.tsx
+++ src/pages/tests/SummaryForm.test.tsx
@@ -42,3 +42,71 @@ describe("SummaryForm component", () => {
     expect(confirmButton).toBeDisabled();
   });
 });
+
+describe("SummaryForm component", () => {
+  test("renders", () => {
+    render(<SummaryForm />);
+  });
+
+  test("check initial conditions", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+    expect(checkbox).not.toBeChecked();
+    expect(confirmButton).toBeDisabled();
+  });
+
+  test("check checkbox enables button", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
+  });
+
+  test("check checkbox disables button", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+    fireEvent.click(checkbox);
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeDisabled();
+  });
+
+  test("check checkbox toggles state", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    fireEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+  });
+
+  test("check button click toggles state", () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", {
+      name: /confirm order/i,
+    });
+    fireEvent.click(checkbox);
+    fireEvent.click(confirmButton);
+    expect(checkbox).not.toBeChecked();
+    expect(confirmButton).toBeDisabled();
+  });
+});
